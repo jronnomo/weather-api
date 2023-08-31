9143d76f75c44620b5922f36184beeff9511f69f
jronnomo <ggronnii@gmail.com>
2023-08-30 22:55:40 -0400

    API to discover station file name using station ID provided in URL

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import pandas as pd
+import glob
+from pathlib import Path
 
-app = Flask("Weather App")
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -11,7 +13,18 @@ def home():
 
 @app.route('/api/<station>/<date>')
 def data(station, date):
-    df = pd.read_csv('data.csv')
-    return render_template('data.html')
+    filepaths = glob.glob('./data/data_small/TG_*')
+    station_df = pd.read_csv("data/data_small/stations.txt", skiprows=17)
+    stations_list = station_df[:100][["STAID", "STANAME                                 "]]
+    station_id = stations_list.loc[stations_list["STAID"] == int(station)]["STAID"].squeeze()
+    if station_id < 10:
+        station_str = '0' + str(station_id)
+    else:
+        station_str = str(station_id)
+    for filepath in filepaths:
+        if Path(filepath).stem == f"TG_STAID0000{station_str}":
+            return filepath
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
